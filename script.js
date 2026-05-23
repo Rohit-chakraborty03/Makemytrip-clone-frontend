a62ff6537158d484f4df797b2835e458b56e64f0
Rohit-chakraborty03 <your-github-email@example.com>
2026-05-23 17:58:58 +0530

    commit

--- script.js
+++ script.js
@@ -4,24 +4,30 @@ const elem3=document.getElementById("multicity");
 
 elem1.addEventListener("click",()=>{
    elem1.style.backgroundColor="#ff8dddab";
-    elem1.style.color="rgb(87, 0, 116)";
     elem1.style.borderRadius="12px";
     elem2.style.backgroundColor="";
     elem3.style.backgroundColor="";
+    elem3.style.opacity=0.5;
+    elem2.style.opacity=0.5;
+    elem1.style.opacity=1;
 })
 
 elem2.addEventListener("click",()=>{
     elem2.style.backgroundColor="#ff8dddab";
-    elem2.style.color="rgb(87, 0, 116)";
     elem2.style.borderRadius="12px";
     elem1.style.backgroundColor="";
     elem3.style.backgroundColor="";
+    elem3.style.opacity=0.5;
+    elem1.style.opacity=0.5;
+    elem2.style.opacity=1;
 })
 
 elem3.addEventListener("click",()=>{
     elem3.style.backgroundColor="#ff8dddab";
-    elem3.style.color="rgb(87, 0, 116)";
     elem3.style.borderRadius="12px";
     elem1.style.backgroundColor="";
     elem2.style.backgroundColor="";
+    elem1.style.opacity=0.5;
+    elem2.style.opacity=0.5;
+    elem3.style.opacity=1;
 })
